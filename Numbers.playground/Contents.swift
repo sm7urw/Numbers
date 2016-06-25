3a33f27afbbfc4a98860dc062d3a43163449a1e2
sm7urw <henrik@korslind.com>
2016-06-25 23:16:04 +0200

    Number exercise

---
+++ Numbers.playground/Contents.swift
@@ -0,0 +1,23 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Integer
+
+var myBankAccount: Int = -500
+
+var my2ndBankAccount: Int = -200
+
+var myAge: UInt = 22
+
+var bigNumber: Int = 214748364
+
+var flyttal:Float = 3.1456644332233
+
+var anotherBankAccount:Double = 55.5
+
+var someVal: Float = 5.5
+
+var sum = myBankAccount * bigNumber
+
+print(sum)
